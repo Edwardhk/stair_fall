0011d439dd840a5c6b52be40afe2993ce91839b0
Edwardhk <hkm727145@gmail.com>
2019-02-06 13:05:34 +0800

    Changed to rect for collision detection

--- stair_fall.js
+++ stair_fall.js
@@ -1,23 +1,24 @@
+var GAME_SPEED = 3;
+
 var BALL_COLOR = [125, 253, 254];
 var BALL_SIZE = 20;
 var BALL_START_Y = 10;
 var BALL_DROP_SPEED = 3;
-var BALL_MOVE_SPEED = 5;
+var BALL_MOVE_SPEED = 3;
 var BALL_MUL = (MUL) => {
     return BALL_START_Y + BALL_SIZE / 2 + BALL_DROP_SPEED * MUL;
 }
 
-var STAIR_WEIGHT = 5;
+var STAIR_THICK = 10;
 var STAIR_COLOR = 255;
 var STAIR_MAX_LEN = 200;
 var STAIR_MIN_LEN = 70;
 var STAIR_START_Y = BALL_MUL(50);
 var STAIR_OFFSET = BALL_DROP_SPEED * 30;
-var STAIR_NUM = 5;
+var STAIR_NUM = 10;
 
 var CANVAS_W = 600;
 var CANVAS_H = BALL_MUL(200);
-var GAME_SPEED = 3;
 
 var CHEATED = 0;
 
@@ -42,15 +43,23 @@ class Ball {
             this.x += BALL_MOVE_SPEED;
     }
 
-    will_drop(stair_coord) {
+    will_drop(stair_rect) {
+        // DROPPED TO BOTTOM
         if (this.y + BALL_SIZE / 2 == height)
             noLoop();
+
+        // CHECK COLLISION
         else {
             this.drop_speed = BALL_DROP_SPEED;
-            for (var i = 0; i < stair_coord.length; i++) {
-                if (this.y + BALL_SIZE / 2 == stair_coord[i][0]) {
-                    if (this.x > stair_coord[i][1] && this.x < stair_coord[i][2])
-                        this.drop_speed = 0;
+            var ball_bot = this.y + BALL_SIZE / 2;
+            for (var i = 0; i < stair_rect.length; i++) {
+                var stair_y = stair_rect[i][0];
+                var start_x = stair_rect[i][1];
+                var end_x = stair_rect[i][1] + stair_rect[i][2];
+
+                if (ball_bot >= stair_y && ball_bot <= stair_y + STAIR_THICK) {
+                    if (this.x >= start_x && this.x <= end_x)
+                        this.drop_speed = -3;
                 }
             }
             this.y += this.drop_speed;
@@ -60,29 +69,31 @@ class Ball {
 
 class Stair {
     constructor() {
-        this.x_arr = [];
+        this.start_x = [];
         this.stair_len_arr = [];
         this.y = STAIR_START_Y;
-        this.stair_coord = [];
+        this.stair_rect = [];
         for (var i = 0; i < STAIR_NUM; i++) {
-            this.x_arr.push(random(0, width - STAIR_MAX_LEN));
+            this.start_x.push(random(0, width - STAIR_MAX_LEN));
             this.stair_len_arr.push(random(STAIR_MIN_LEN, STAIR_MAX_LEN));
         }
     }
 
     display() {
-        var stair_coord = [];
-        strokeWeight(STAIR_WEIGHT);
+        var stair_rect = [];
         stroke(STAIR_COLOR);
 
         for (var i = 0; i < STAIR_NUM; i++) {
             var stair_y = this.y + STAIR_OFFSET * i;
             var stair_len = this.stair_len_arr[i];
-            line(this.x_arr[i], stair_y, this.x_arr[i] + stair_len, stair_y);
-            this.stair_coord.push([stair_y, this.x_arr[i], this.x_arr[i] + stair_len]);
+            rect(this.start_x[i], stair_y, stair_len, STAIR_THICK);
+            if (stair_y > 0) {
+                this.stair_rect.push([stair_y, this.start_x[i], stair_len]);
+            }
         }
+        this.y -= GAME_SPEED;
 
-        return stair_coord;
+        return stair_rect;
     }
 }
 
@@ -99,8 +110,8 @@ function draw() {
     noFill();
     strokeWeight(5);
     stair.display();
-    console.log(ball.y + BALL_SIZE / 2);
-    ball.will_drop(stair.stair_coord);
+
+    ball.will_drop(stair.stair_rect);
     if (keyIsPressed)
         ball.move();
     ball.display();
